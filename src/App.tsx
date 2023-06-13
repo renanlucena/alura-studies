9d6b81540225870bdce0b70d8b8e40b9938b4aba
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-13 15:40:18 -0300

    implementando o Formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Botao from './componentes/Botao';
+import Formulario from './componentes/Formulario';
+
 
 
 function App() {
   return (
     <div className="App">
-     
+      <Formulario />
     </div>
   );
 }
