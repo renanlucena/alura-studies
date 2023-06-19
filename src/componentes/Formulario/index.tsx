d7519bc500d1f16e4f4d3992b456f601e8615500
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-19 13:59:02 -0300

    dinamizando tarefa e tempo

--- src/componentes/Formulario/index.tsx
+++ src/componentes/Formulario/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Botao from '../Botao'
 import styles from "./Formulario.module.scss";
 
 export default function Formulario() {
+    const [tarefa, setTarefa] = useState("");
+    const [tempo, setTempo] = useState("00:00:00");
+
+    function adicionarTarefa(e: React.FormEvent){
+        e.preventDefault();
+        console.log('state: ', tarefa)
+    }
     return (
-        <form className={styles.novaTarefa}>
+        <form className={styles.novaTarefa} onSubmit={adicionarTarefa}>
             <div className={styles.inputContainer}>
                 <label htmlFor='tarefa'>
                     Adicione um novo estudo
                 </label>
                 <input 
                     type='text'
+                    value={tarefa}
+                    onChange={e => setTarefa(e.target.value)}
                     name='tarefa'
                     id='tarefa'
                     placeholder='O que você quer estudar?'
@@ -24,6 +33,8 @@ export default function Formulario() {
                 </label>
                 <input 
                     type='time'
+                    value={tempo}
+                    onChange={e => setTempo(e.target.value)}
                     step= '1'
                     name='tempo'
                     id='tempo'
