5f895b7c8fc6ac657b775651d5d2b628b2a04409
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-19 13:07:56 -0300

    removendo um contexto

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { TarefasContext, TarefasProvider } from "./common/context/Tarefas";
 import App from "./paginas/App";
-
 export default function AppRoutes(){
     return(
         <BrowserRouter>
-            <TarefasProvider>
                 <Routes>
                     <Route path="/" element={<App/>}/> 
                 </Routes>
-            </TarefasProvider>
+        
         </BrowserRouter>
     )
 }
